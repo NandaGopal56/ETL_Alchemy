f601992b2c07b91443863704216dc638d379d3f7
Nanda Gopal Pattanayak <40311776+NandaGopal56@users.noreply.github.com>
2023-04-30 00:47:22 +0530

    psuedo code for get_connection_metadata

--- databases/views.py
+++ databases/views.py
@@ -26,6 +26,21 @@ def get_connection_metadata(request, connection_name):
 
     if connectionObj.type == 'mysql':
         conn = get_mysql_connection(connection_name, connectionObj)
-        
+
+        requirement = request.POST.get('requirement')
+        if requirement == 'GET_TABLES':
+            cursor = conn.cursor()
+            cursor.execute("SHOW TABLES;")
+            data = cursor.fetchall()
+            cursor.close()
+            return data 
+        elif requirement == 'GET_TABLE_DETAILS':
+            table_name = request.POST.get('table_name')
+            cursor = conn.cursor()
+            cursor.execute("use table_name")
+            cursor.execute("SHOW TABLES DETAILS;")
+            data = cursor.fetchall()
+            cursor.close()
+            return data 
 
     return HttpResponse("OK")
